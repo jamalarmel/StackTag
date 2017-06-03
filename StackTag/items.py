4321dba3cca58ea0335315f8a8c937c64dbc2f17
Jamal Armel <jamalarmel@gmail.com>
2017-06-03 21:43:52 +0300

    crawl python tags

--- StackTag/items.py
+++ StackTag/items.py
@@ -11,4 +11,6 @@ import scrapy
 class StacktagItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
+    title = scrapy.Field()
+    url = scrapy.Field()
     pass
--- StackTag/spiders/stack_crawler.py
+++ StackTag/spiders/stack_crawler.py
@@ -2,20 +2,25 @@
 import scrapy
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
+from StackTag.items import StacktagItem
 
 
 class StackCrawlerSpider(CrawlSpider):
     name = 'stack_crawler'
     allowed_domains = ['stackoverflow.com']
-    start_urls = ['http://stackoverflow.com/']
+    #start_urls = ['http://stackoverflow.com/questions?pagesize=50&sort=newest']
+    start_urls = ['https://stackoverflow.com/questions/tagged/python?page=1&sort=votes&pagesize=15']
 
     rules = (
-        Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
+        #Rule(LinkExtractor(allow=r'questions\?page=[0-9]&sort=votes'),callback='parse_item', follow=True),
+        Rule(LinkExtractor(allow=r'questions/tagged/python\?page=[0-9]&sort=votes'),callback='parse_item', follow=True),
     )
 
     def parse_item(self, response):
-        i = {}
-        #ii['domain_id'] = response.xpath('//input[@id="sid"]/@value').extract()
-        #i['name'] = response.xpath('//div[@id="name"]').extract()
-        #i['description'] = response.xpath('//div[@id="description"]').extract()
-        return i
+        questions = response.xpath('//div[@class="summary"]/h3')
+
+        for question in questions:
+            item = StacktagItem()
+            item['url'] = question.xpath('a[@class="question-hyperlink"]/@href').extract()[0]
+            item['title'] = question.xpath('a[@class="question-hyperlink"]/text()').extract()[0]
+            yield item
