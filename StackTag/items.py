563547fb6a9239258223c6a092fc2f1e685af76a
Jamal Armel <jamalarmel@gmail.com>
2017-06-04 14:24:02 +0300

    Updated Crawler

--- StackTag/items.py
+++ StackTag/items.py
@@ -12,5 +12,6 @@ class StacktagItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     title = scrapy.Field()
-    url = scrapy.Field()
+    link = scrapy.Field()
+    qbody = scrapy.Field()
     pass
--- StackTag/spiders/stack_crawler.py
+++ StackTag/spiders/stack_crawler.py
@@ -8,20 +8,21 @@ from StackTag.items import StacktagItem
 class StackCrawlerSpider(CrawlSpider):
     name = 'stack_crawler'
     allowed_domains = ['stackoverflow.com']
-    #start_urls = ['http://stackoverflow.com/questions?pagesize=50&sort=newest']
     start_urls = ['https://stackoverflow.com/questions/tagged/python?page=1&sort=votes&pagesize=15']
-
     rules = (
-        #Rule(LinkExtractor(allow=r'questions\?page=[0-9]&sort=votes'),callback='parse_item', follow=True),
         Rule(LinkExtractor(allow=r'questions/tagged/python\?page=[0-9]&sort=votes'),callback='parse_item', follow=True),
-        #Rule(LinkExtractor(allow=r'questions/tagged/python\?page=[0-9]&sort=votes'),callback='parse_item', follow=False)
     )
 
-    def parse_item(self, response):
-        questions = response.xpath('//div[@class="summary"]/h3')
+    BASE_URL = 'https://stackoverflow.com/'
 
-        for question in questions:
-            item = StacktagItem()
-            item['url'] = question.xpath('a[@class="question-hyperlink"]/@href').extract()[0]
-            item['title'] = question.xpath('a[@class="question-hyperlink"]/text()').extract()[0]
-            yield item
+    def parse(self, response):
+        links = response.xpath('//a[@class="question-hyperlink"]/@href').extract()
+        for link in links:
+            absolute_url = self.BASE_URL + link
+            yield scrapy.Request(absolute_url, callback=self.parse_attr)
+
+    def parse_attr(self, response):
+        item = StacktagItem()
+        item["link"] = response.url
+        item["qbody"] = "".join(response.xpath('//div[@class="post-text"]/p/text()').extract())
+        return item
